1f6ec8c6c72d4a7575db648a98e9fc335288cc20
Bipin <bipinmane2@gmail.com>
2025-11-07 22:44:14 +0000

    Addition class test_push

---
+++ src/BasicAddition_Test.java
@@ -0,0 +1,11 @@
+public class BasicAddition_Test {
+
+    public static void main(String[] args) {
+
+        int ab = 224;
+        int bc = 225;
+        long cd = 225;
+
+        System.out.println(ab+bc+cd);
+    }
+}
